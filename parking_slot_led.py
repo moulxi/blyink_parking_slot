cfb389b09da5e679439bf80f3ec051e93b8f8e6c
moulxi <alsk124215545@gmail.com>
2024-11-13 21:08:01 +0800

    can send led signal

---
+++ parking_slot_led.py
@@ -0,0 +1,37 @@
+
+import requests
+import time
+import re
+
+class SlotGroupError(Exception):
+    pass
+
+
+class SlotGroup:
+    
+    def __init__(self, token:str):
+        self.token = token
+    
+    def send_led_sig(self, pin:str, sig:int):
+
+        # verify if "pin" is valid
+        pattern = r"^v[0-9]$"
+        if not isinstance(pin, str) or not re.match(pattern, pin):
+            raise SlotGroupError("send_led_sig(pin, sig), v_pin must be 'v0', 'v1', ..., 'v9' of type 'str'")
+        
+        # verify if "sig" is valid
+        if not isinstance(sig, int) or sig not in (0, 1):
+            raise SlotGroupError("send_led_sig(pin, sig), sig must be 0 or 1 of type 'int'")
+
+        # send the signal by get method
+        url = f'https://blynk.cloud/external/api/update?token={self.token}&{pin}={sig}'
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f'Sent {sig} to {pin}')
+            else:
+                print(f'Failed to send the signal： {response.status_code},  {response.text}')
+        except requests.exceptions.RequestException as e:
+            print(f'Somthing went wrong while sending a request : {e}')
+
+    
